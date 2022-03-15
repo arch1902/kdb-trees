7cced46e4a649991c281cc2045cf5de032acbc97
Arpit Chauhan <achauhan1902@gmail.com>
2022-03-15 10:27:44 +0530

    Add files via upload

--- kdbtree.cpp
+++ kdbtree.cpp
@@ -1,8 +1,10 @@
-#include<iostream>
+#include <iostream>
 #include "file_manager.h"
 #include "errors.h"
-#include<cstring>
-#include<bits/stdc++.h>
+#include <cstring>
+#include <vector>
+#include <fstream>
+#include <map>
 
 using namespace std;
 
@@ -10,8 +12,13 @@ struct Node{
 	int node_type; // 0 for point page, 1 for region page
 	int split_dim; 
 	int page_identifier;
-    vector<pair<vector<int>,int>> regions;
+    vector<pair<vector<int>,int>> regions; //points in case of point node
 };
+
+bool operator<(const PageHandler &l1, const PageHandler &l2)
+        {
+                return false;
+        }
 
 int point_size;
 int region_size;
@@ -34,28 +41,84 @@ int store(struct Node node, PageHandler &page, int offset, bool flag){
 	}
 
 	if(node.node_type==0){
-		for(int i=0;i<d;i++){
-			memcpy (&data[offset], &node.regions[i], sizeof(int));
+		for(auto region : node.regions){
+			for(int i=0;i<d;i++){
+				memcpy (&data[offset], &region.first[i], sizeof(int));
+				offset+=4;
+			}
+			memcpy (&data[offset], &region.second, sizeof(int));
 			offset+=4;
 		}
-		int location;
-		memcpy (&data[offset], &location, sizeof(int));
-		offset+=4;
 	}else {
-		for(int i=0;i<2*d;i++){
-			memcpy (&data[offset], &node.regions[i], sizeof(int));
+		for(auto region : node.regions){
+			for(int i=0;i<2*d;i++){
+				memcpy (&data[offset], &region.first[i], sizeof(int));
+				offset+=4;
+			}
+			memcpy (&data[offset], &region.second, sizeof(int));
 			offset+=4;
 		}
-		int location;
-		memcpy (&data[offset], &location, sizeof(int));
-		offset+=4;
 	}
-
 	return offset;
-
 }
 
-void insert(vector<int> &point, PageHandler &root, fstream &fsout){
+int addPoint(vector<int> &point, PageHandler &page, int offset){
+	char *data = page.GetData ();
+	for(int i=0;i<d;i++){
+		memcpy (&data[offset], &point[i], sizeof(int));
+		offset+=4;
+	}
+	int location;
+	memcpy (&data[offset], &location, sizeof(int));
+	offset+=4;
+	occupancy[page]++;
+	return offset;
+}
+
+// load function 
+
+struct Node load(PageHandler &page){
+	struct Node node;
+	char *data = page.GetData ();
+	memcpy (&node.page_identifier, &data[0], sizeof(int));
+	memcpy (&node.node_type, &data[4], sizeof(int));
+	memcpy (&node.split_dim, &data[8], sizeof(int));
+	int offset = 12;
+	if(node.node_type==0){
+		vector<pair<vector<int>,int>> points;
+		for(int i=0;i<occupancy[page];i++){
+			vector<int> point(d,0);
+			for(int j=0;j<d;j++){
+				memcpy (&point[j], &data[offset], sizeof(int));
+				offset+=4;
+			}
+			int location;
+			memcpy (&location, &data[offset], sizeof(int));
+			offset+=4;
+			points.push_back({point,location});
+		}
+		node.regions = points;
+
+	}else {
+		vector<pair<vector<int>,int>> regions;
+		for(int i=0;i<occupancy[page];i++){
+			vector<int> region(2*d,0);
+			for(int j=0;j<2*d;j++){
+				memcpy (&region[j], &data[offset], sizeof(int));
+				offset+=4;
+			}
+			int cid;
+			memcpy (&cid, &data[offset], sizeof(int));
+			offset+=4;
+			regions.push_back({region,cid});
+		}
+		node.regions = regions;
+	}
+
+	return node;
+}
+
+void insert(vector<int> &point, PageHandler &root){
 	if(occupancy[root]==0){
 		pages[num_pages++] = root;
 		struct Node node;
@@ -63,20 +126,37 @@ void insert(vector<int> &point, PageHandler &root, fstream &fsout){
 		node.node_type = 0;
 		node.split_dim = 0;
 		node.regions.push_back({point,0});
-		store(node,root,0,true);
+		occupancy[root]+=1;
+		int offset = store(node,root,0,true);
 
 	}else {
-
+		struct Node node = load(root);
+		if(node.node_type==0){
+			if(occupancy[root]<max_points){
+				addPoint(point,root,occupancy[root]*point_size + 12);
+			}
+			// else 
+			// {
+			// 	// handle overflow
+			// }
+		}else {
+			for(auto region : node.regions){
+				bool flag = true;
+				for(int i=0;i<d;i++){
+					if(region.first[i]>point[i] or region.first[d+i]<=point[i]){
+						flag = false;
+						break;
+					}
+				}
+				if(flag) {
+					insert(point,pages[region.second]);
+				}
+			}
+		}
 	}
 }
 
-// void pquery(vector<int> point, fstream &fsout){
 
-// }
-
-// void rquery(vector<int> point_min, vector<int> point_max, fstream &fsout){
-
-// }
 
 // void reorganize(node Node){
 
@@ -86,9 +166,40 @@ void insert(vector<int> &point, PageHandler &root, fstream &fsout){
 
 // }
 
+bool pquery(vector<int>& point, PageHandler &page){
+	struct Node node = load(page);
+	if(node.node_type==0){
+		for(auto p: node.regions){
+			bool flag = true;
+			for(int i=0;i<d;i++){
+				if(p.first[i]!=point[i]){
+					flag = false;
+					break;
+				}
+			}
+			if(flag) return true;
+		}
+		return false;
+	}else {
+		for(auto region : node.regions){
+			bool flag = true;
+			for(int i=0;i<d;i++){
+				if(region.first[i]>point[i] or region.first[d+i]<=point[i]){
+					flag = false;
+					break;
+				}
+			}
+			if(flag) {
+				pquery(point,pages[region.second]);
+			}
+		}
+	}
 
+}
 
+// void rquery(vector<int> point_min, vector<int> point_max, fstream &fsout){
 
+// }
 
 
 int main(int argc, char* argv[]) {
@@ -111,7 +222,7 @@ int main(int argc, char* argv[]) {
 	num_pages = 0;
 
 	FileManager fm;
-	FileHandler fh = fm.CreateFile("f1.txt");
+	FileHandler fh = fm.CreateFile("temp.txt");
 	PageHandler root =  fh.NewPage();
 	occupancy[root] = 0;
 	
@@ -123,8 +234,12 @@ int main(int argc, char* argv[]) {
 	// INSERT -78 -29
 	// INSERT 62 18
 
-	// vector<int> p1 = {56,37};
-	// insert(p1,)
+	vector<int> p1 = {56,37};
+	vector<int> p2 = {56,38};
+	insert(p1,root);
+	insert(p2,root);
+	cout<< pquery(p1,root)<<endl;
+	cout<< pquery(p2,root)<<endl;
 	
 
 	// if (fsin.is_open()){ 
@@ -170,6 +285,9 @@ int main(int argc, char* argv[]) {
 	// 	fsin.close();
 	// 	fsout.close();
    	// }
+
+	fm.CloseFile (fh);
+	fm.DestroyFile ("temp.txt");
 
 
 }
